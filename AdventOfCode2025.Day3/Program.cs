15e104bb4bd1b5f447df1affe5f2e2a1d297c5dc
Lucas Dell'Isola <lucas.dellisola@rystadenergy.com>
2025-12-03 09:27:37 +0100

    Day 3

---
+++ AdventOfCode2025.Day3/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+var input = File.ReadAllLines("input.txt");
+
+var result = 
+input
+    .AsParallel()
+    .Select(Day2)
+    .Sum();
+
+Console.WriteLine("-------------");
+Console.WriteLine(result);
+return;
+
+static long Day2(string batteryBank)
+{
+    const int batteriesNeeded = 12;
+    
+    int[] indexes = new int[batteriesNeeded];
+    
+    for (int i = 0; i < batteriesNeeded; i++)
+    {
+        int previousIndex = i is 0 ? 0 : indexes[i - 1] + 1;
+        indexes[i] = IndexOfMax(batteryBank, batteryBank.Length - batteriesNeeded + i+1, previousIndex);
+    }
+
+    var selectedBatteries = string.Join("", indexes.Select(t=> batteryBank[t]));
+    // Console.WriteLine(selectedBatteries);
+    return long.Parse(selectedBatteries);
+}
+static int Day1(string batteryBank)
+{
+    var firstBatteryIndex = IndexOfMax(batteryBank, batteryBank.Length - 1, 0);
+    var secondBatteryIndex = IndexOfMax(batteryBank, batteryBank.Length, firstBatteryIndex + 1);
+
+    return int.Parse($"{batteryBank[firstBatteryIndex]}{batteryBank[secondBatteryIndex]}");
+}
+
+static int IndexOfMax(ReadOnlySpan<char> array, int stopPosition, int startPosition = 0)
+{
+    int? highestIndex = null;
+
+    for (int i = startPosition; i < stopPosition; i++)
+    {
+        highestIndex ??= i;
+        
+        if (array[i] > array[highestIndex.Value])
+            highestIndex = i;
+
+        if (array[i] == '9')
+            return i;
+
+    }
+
+    return highestIndex!.Value;
+}
