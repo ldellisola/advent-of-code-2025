2051452ed1a424af3ac43705a1df857f36afbb37
Lucas Dell'Isola <lucas.dellisola@rystadenergy.com>
2025-12-05 10:51:53 +0100

    Day 5 part 2

--- AdventOfCode2025.Day5/Program.cs
+++ AdventOfCode2025.Day5/Program.cs
@@ -1,7 +1,6 @@
-﻿using System.Runtime.InteropServices.ComTypes;
-
+﻿
 var input = File.ReadAllText("input.txt")
-    .Split("\n\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+    .Split("\r\n\r\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
 
 
 var result = Day2(input);
@@ -19,50 +18,61 @@ static long Day2(string[] input)
         .Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
         .Select(t => t.Split('-', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
         .Select(t => (Min: long.Parse(t[0]), Max: long.Parse(t[1])))
-        .SelectMany<(long Min, long Max),Edge>(t=> [ new Edge(t.Min, EdgeType.Min), new Edge(t.Max, EdgeType.Max)])
-        .OrderBy(t=> t.Number)
+        .OrderBy(t=> t.Min)
         .ToArray();
-
-    long total = 0;
-    long? min = null;
-    long? max = null;
     
-    for (int i = 0; i < vegetableDb.Length; i++)
+    do
     {
-        switch ((min, max))
+        vegetableDb = CalculateNewRanges(vegetableDb);
+        if (!HasIntersections(vegetableDb))
+            break;
+    }
+    while(true);
+
+
+    return vegetableDb.Select(t=> 1+ t.Max - t.Min).Sum();
+
+}
+
+
+static bool HasIntersections((long Min, long Max)[] ranges)
+{
+    return ranges
+        .SelectMany(t => new[] { (Num: t.Min, type: "min"), (Num: t.Max, type: "max") })
+        .OrderBy(t => t.Num)
+        .Chunk(2)
+        .Any(t => t[0].type == t[1].type);
+}
+
+
+static (long Min, long Max)[] CalculateNewRanges((long Min, long Max)[] ranges)
+{
+    var newRanges = new HashSet<(long Min, long Max)>();
+    var alreadyIntersected = new HashSet<(long Min, long Max)>();
+    
+    for (int i = 0; i < ranges.Length; i++)
+    {
+        var currentRange = ranges[i];
+        if (alreadyIntersected.Contains(currentRange))
+            continue;
+        bool hasIntersected = false;
+        for (int j = 0 ; j < ranges.Length; j++)
         {
-            case (null,_): 
-                min = vegetableDb[i].Number;
-                if (vegetableDb[i].Type is not EdgeType.Min) throw new Exception();
-                continue;
-            case (_, null):
-                if (vegetableDb[i].Type is EdgeType.Min)
-                    continue;
-                if (vegetableDb[i].Type is EdgeType.Max)
-                    max = vegetableDb[i].Number;
-                break;
-            case (_,_):
-                if (vegetableDb[i].Type is EdgeType.Max)
-                {
-                    max = vegetableDb[i].Number;
-                    continue;
-                }
+            bool intersects = (currentRange.Min <= ranges[j].Min && ranges[j].Min <= currentRange.Max) || (currentRange.Min <= ranges[j].Max && ranges[j].Max <= currentRange.Max);
 
-                if (vegetableDb[i].Type is EdgeType.Min)
-                {
-                    total += 1+ max.Value - min.Value;
-                    min = vegetableDb[i].Number;
-                    max = null;
-                }
-
+            if (intersects && currentRange != ranges[j])
+            {
+                alreadyIntersected.Add(ranges[j]);
+                hasIntersected = true;
+                newRanges.Add((Math.Min(currentRange.Min, ranges[j].Min), Math.Max(currentRange.Max, ranges[j].Max)));
                 break;
+            }
         }
-        
-        
+        if (!hasIntersected)
+            newRanges.Add(currentRange);
     }
 
-    return total;
-
+    return newRanges.OrderBy(t=> t.Min).ToArray();
 }
 
 static long Day1(string[] input)
@@ -96,12 +106,4 @@ static bool IsVegetableFresh((long Min, long Max)[] freshVegetableDb, long veget
     }
 
     return false;
-}
-
-record Edge(long Number, EdgeType Type);
-
-enum EdgeType
-{
-    Min,
-    Max
 }
